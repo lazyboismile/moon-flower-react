f76acbd792f475ec6dc72c8d1760a00255c4a55d
lazyboismile <lazyboijon@gmail.com>
2025-09-02 11:47:20 +0500

    feat: develop Socket.io Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import App from "./app/App";
-import reportWebVitals from "./reportWebVitals";
-import "./css/index.css";
-import theme from "./app/MaterialTheme";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./app/App";
 import ContextProvider from "./app/context/ContextProvider";
+import { SocketProvider } from "./app/context/SocketContext";
+import theme from "./app/MaterialTheme";
+import { store } from "./app/store";
+import "./css/index.css";
+import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -18,12 +19,14 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ContextProvider>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Router>
-            <App />
-          </Router>
-        </ThemeProvider>
+        <SocketProvider>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <Router>
+                <App />
+              </Router>
+            </ThemeProvider>
+          </SocketProvider>
       </ContextProvider>
     </Provider>
   </React.StrictMode>,
